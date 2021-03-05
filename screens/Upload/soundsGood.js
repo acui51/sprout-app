abefe70e7a96a04869e89c1563852ca7cc6aa49f
Jialin <zhuoj@bxcience.edu>
2021-03-04 20:52:35 -0500

    justify content on soundsgood page

--- screens/Upload/soundsGood.js
+++ screens/Upload/soundsGood.js
@@ -11,19 +11,19 @@ import Container from "../../hoc/Container";
 export default function soundsGood({ navigation }) {
 
   return (
-    <Container style={styles.container}>
+    <Container customStyle={styles.container}>
       <CustomText customStyles={styles.title}>Sounds Good?</CustomText>
-      <View>
+      <View style={styles.container}>
         <Image source = {Images.sound_freq}/>
-      </View>
-      <View style={styles.button}>
-        <CustomButton
-          text="YES! CONTINUE"
-          variantButton="primaryShadow"
-          variantText="whiteText"
-          width={196}
-          onPress={() => navigation.goBack()}
-        />
+        <View style={styles.button}>
+          <CustomButton
+            text="YES! CONTINUE"
+            variantButton="primaryShadow"
+            variantText="whiteText"
+            width={196}
+            onPress={() => navigation.goBack()}
+          />
+        </View>
       </View>
     </Container>
   );
@@ -41,6 +41,11 @@ const styles = StyleSheet.create({
     color: Colors.white,
     paddingTop: 40,
     paddingBottom: 100,
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
     padding: 10,
