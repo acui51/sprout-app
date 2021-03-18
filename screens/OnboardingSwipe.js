c2604b36fa44585370089e60a5d2a0eaae5f4f60
Jialin <zhuoj@bxcience.edu>
2021-03-18 05:25:40 -0400

    Update OnboardingSwipe.js

---
+++ screens/OnboardingSwipe.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Image, View, StatusBar, Dimensions, Button, TouchableOpacity } from "react-native";
+import Swiper from "react-native-swiper";
+import CustomButton from "../components";
+
+const { width, height } = Dimensions.get("window");
+export default ( ) => {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {};
+    // }
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden={true} />
+      <Swiper loop={false} showsButtons={true}>
+        <View>
+          <Image
+            style={styles.slide}
+            source={require("../assets/Images/onboarding/Discover.png")}
+          />
+        </View>
+        <View>
+          <Image
+            style={styles.slide}
+            source={require("../assets/Images/onboarding/Add.png")}
+          />
+        </View>
+        <View>
+          <Image
+            style={styles.slide}
+            source={require("../assets/Images/onboarding/Seed.png")}
+          />
+        </View>
+        <View>
+          <Image
+            style={styles.slide}
+            source={require("../assets/Images/onboarding/Evolve.png")}
+          />
+        </View>
+        <View>
+          <View>
+              <TouchableOpacity>
+            <Image
+              style={styles.slide}
+              source={require("../assets/Images/onboarding/Connect.png")}
+            />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Swiper>
+      <View>
+      <Button
+          title="START"
+        />
+      </View>
+    </View>
+  );
+};
+
+var styles = {
+  container: {
+    flex: 1,
+  },
+  slide: {
+    height: "100%",
+    width: "100%",
+  },
+};
