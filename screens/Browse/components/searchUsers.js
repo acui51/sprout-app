e7821e9d28b8ecc1c4285d3bc607f95e8a0e6905
Alix <acui@stanford.edu>
2021-03-19 00:03:30 -0400

    update search

--- screens/Browse/components/searchUsers.js
+++ screens/Browse/components/searchUsers.js
@@ -1,10 +1,11 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect, useEffect, useState, useRef } from "react";
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
@@ -20,12 +21,17 @@ import UserCard from "../../Profile/components/UserCard";
 const SearchUsers = () => {
   const [searchText, setSearchText] = useState("");
   const navigation = useNavigation();
+  const searchRef = useRef(null);
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, [navigation]);
+
+  useEffect(() => {
+    searchRef.current.focus();
+  }, []);
 
   return (
     <Container>
@@ -43,6 +49,7 @@ const SearchUsers = () => {
             }
           />
           <TextInput
+            ref={searchRef}
             style={styles.searchText}
             placeholder="Search for users"
             placeholderTextColor={Colors.gray}
@@ -70,20 +77,71 @@ const SearchUsers = () => {
           </TouchableOpacity>
         </View>
       </View>
-      {searchText === ("Honest Ocean" || "brunetted" || "Brunetted") && (
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate("Profile", { profile: "honest_ocean" })
-          }
-        >
-          <UserCard
-            username="brunetted"
-            username2="honest_ocean"
-            text="Honest Ocean"
-            pfp={Images.honest_ocean}
-          />
-        </TouchableOpacity>
-      )}
+      <ScrollView>
+        {searchText === ("Honest Ocean" || "brunetted" || "Brunetted") ? (
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("Profile", { profile: "honest_ocean" })
+            }
+          >
+            <UserCard
+              username="brunetted"
+              username2="honest_ocean"
+              text="Honest Ocean"
+              pfp={Images.honest_ocean}
+            />
+          </TouchableOpacity>
+        ) : (
+          <>
+            <CustomText customStyles={styles.subheading}>RECENT</CustomText>
+            <View style={styles.line}></View>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.push("Profile", { profile: "scissors_s" })
+              }
+            >
+              <UserCard
+                username="scissors_s"
+                username2="scissors_s"
+                text="Scissors"
+                pfp={Images.scissors}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.push("Profile", { profile: "bessie_b" })
+              }
+            >
+              <UserCard
+                username="bessie_b"
+                username2="bessie_b"
+                text="Bessie b"
+                pfp={Images.bessie_b}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.push("Profile", { profile: "justin_t" })
+              }
+            >
+              <UserCard
+                username="justin_t"
+                username2="justin_t"
+                text="Justin Timberpound"
+                pfp={Images.justin_t}
+              />
+            </TouchableOpacity>
+            <CustomText customStyles={styles.subheading}>RECENT</CustomText>
+            <View style={styles.line}></View>
+            <UserCard
+              username="dj_cobra"
+              username2="dj_cobra"
+              text="DJ Cobra"
+              pfp={Images.dj_cobra}
+            />
+          </>
+        )}
+      </ScrollView>
     </Container>
   );
 };
@@ -103,5 +161,17 @@ const styles = StyleSheet.create({
   searchText: {
     color: Colors.white,
     fontSize: 14,
+  },
+  subheading: {
+    fontSize: 10,
+    fontWeight: "bold",
+    color: Colors.gray,
+    marginBottom: 20,
+    marginTop: 5,
+  },
+  line: {
+    borderBottomColor: Colors.background2,
+    borderBottomWidth: 1,
+    marginBottom: 12,
   },
 });
